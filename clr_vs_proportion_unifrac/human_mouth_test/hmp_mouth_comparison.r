316707cf36fcc7f0d3fb5c9a69372e5813469f61
ruthgrace <ruthgracewong@gmail.com>
2014-11-16 20:48:32 -0500

    started mouth comparison script, need to figure out how to get tree

--- clr_vs_proportion_unifrac/human_mouth_test/hmp_mouth_comparison.r
+++ clr_vs_proportion_unifrac/human_mouth_test/hmp_mouth_comparison.r
@@ -1,4 +1,17 @@
-data <- read.table("hmp_mouth_data.txt",sep="\t",header=TRUE,row.names=1)
+
+library(ape)
+library(phangorn)
+
+mouth.otu <- read.table("hmp_mouth_data.txt",sep="\t",header=TRUE,row.names=1)
 
 groups <- as.factor(c(rep("bm",20),rep("td",20),rep("akg",20),rep("hp",20),rep("s",20)))
 
+source("../../CLRUniFrac.R")
+source("../../GUniFrac.R")
+
+mouth.otu <- t(data)
+
+#HOW DO I GET THE TREE
+
+clrUnifrac <- CorrectCLRUniFrac(mouth.otu, mouth.tree, alpha = c(1))$unifrac[,,1]
+gUnifrac <- GUniFrac(mouth.otu, mouth.tree, alpha = c(1))$unifrac[,,1]
