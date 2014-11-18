ad8aea6b98c602440c0bfe5cd5cfcb2a60acb940
ruthgrace <ruthgracewong@gmail.com>
2014-11-17 20:26:31 -0500

    added dendogram + bar plots (for poster, need to group non lacto by genus in bar plots)

--- clr_vs_proportion_unifrac/brazil_study_test/pcoa_test_script.r
+++ clr_vs_proportion_unifrac/brazil_study_test/pcoa_test_script.r
@@ -3,6 +3,9 @@
 library(ape)
 library(phangorn)
 
+# get default par
+plotParameters <- par()
+
 #I hacked the GUniFrac script to use CLR weighting instead of proportional abundance rating
 source("../../CLRUniFrac.R")
 
@@ -11,9 +14,16 @@ source("../../GUniFrac.R")
 
 # read OTU table and format appropriately for input into UniFrac methods
 brazil.otu.tab <- read.table("./brazil_study_data/td_OTU_tag_mapped_RDPlineage_blastcorrected_vvcfilter_tempgenera.txt", header=T, sep="\t", row.names=1, comment.char="", check.names=FALSE)
+
+#remove taxonomy column to make otu count matrix numeric
 taxonomy <- brazil.otu.tab$taxonomy
 brazil.otu.tab <- brazil.otu.tab[-length(colnames(brazil.otu.tab))]
 brazil.otu.tab <- t(as.matrix(brazil.otu.tab))
+
+#sort taxa from most to least abundant
+taxaOrder <- rev(order(apply(brazil.otu.tab,2,sum)))
+taxonomy <- taxonomy[taxaOrder]
+brazil.otu.tab <- brazil.otu.tab[,taxaOrder]
 
 # read and root tree (rooted tree is required)
 brazil.tree <- read.tree("./brazil_study_data/fasttree_all_seed_OTUs.tre")
@@ -111,20 +121,21 @@ brazil.prop <- t(apply(brazil.otu.tab,1,function(x) x/sum(x)))
 #save plots as PDF
 pdf("test_plots_with_brazil_study_data.pdf")
 
-#plot dendogram with bar plots -- NEED TO FIX COLORS
-palette("default")
+#plot dendogram with bar plots
+colors <- c("steelblue3","skyblue1", "indianred1", "mediumpurple1", "olivedrab3", "pink", "#FFED6F", "mediumorchid3", "ivory2", "tan1", "aquamarine3", "#C0C0C0", "royalblue4", "mediumvioletred", "#999933", "#666699", "#CC9933", "#006666", "#3399FF", "#993300", "#CCCC99", "#666666", "#FFCC66", "#6699CC", "#663366", "#9999CC", "#CCCCCC", "#669999", "#CCCC66", "#CC6600", "bisque", "#9999FF", "#0066CC", "#99CCCC", "#999999", "#FFCC00", "#009999", "#FF9900", "#999966", "#66CCCC", "#339966", "#CCCC33", "#EDEDED")
+
 #GG legacy code. Fix size, margins, position
 par(mfrow=c(2,1), mar=c(1, 3, 2, 1) + 0.1)
 plot(ruthClrUnifrac.dendo, axes=F, ylab=NULL, ann=F)
 #order the barplot 
-barplot(t(brazil.prop[ruthClrUnifrac.dendo$order,]), space=0, las=2)
+barplot(t(brazil.prop[ruthClrUnifrac.dendo$order,]), space=0,col=colors, las=2)
 
 par(mfrow=c(2,1), mar=c(1, 3, 2, 1) + 0.1)
 plot(gUnifrac.dendo, axes=F, ylab=NULL, ann=F)
 #order the barplot 
-barplot(t(brazil.prop[gUnifrac.dendo$order,]), space=0, las=2)
+barplot(t(brazil.prop[gUnifrac.dendo$order,]), space=0,col=colors, las=2)
 
-
+par(plotParameters)
 
 
 #for this data set, the colors represent
